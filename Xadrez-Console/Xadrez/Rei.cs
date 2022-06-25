a36cc070dc77437118d6fb3a88df15c31a2948f7
Lucas Carvalho <dev8@automatizasistemas.com.br>
2022-06-25 14:38:18 -0300

    Começando a colocar peças no tabuleiro

---
+++ Xadrez-Console/Xadrez/Rei.cs
@@ -0,0 +1,17 @@
+﻿using Xadrez_Console.TabuleiroXadrez;
+
+namespace Xadrez_Console.Xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+
+    }
+}
---
+++ Xadrez-Console/Xadrez/Torre.cs
@@ -0,0 +1,17 @@
+﻿using Xadrez_Console.TabuleiroXadrez;
+
+namespace Xadrez_Console.Xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+
+    }
+}
