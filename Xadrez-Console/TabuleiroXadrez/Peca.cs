dfa9dee955878552f38e63421f14d1bf37575237
Lucas Carvalho <dev8@automatizasistemas.com.br>
2022-06-26 14:41:49 -0300

    Mudando o nome do método de moverPara para MovimentoPossivel

--- Xadrez-Console/TabuleiroXadrez/Peca.cs
+++ Xadrez-Console/TabuleiroXadrez/Peca.cs
@@ -29,7 +29,7 @@
             return false;
         }
 
-        public bool PodeMoverPara(Posicao posicao)
+        public bool MovimentoPossivel(Posicao posicao)
         {
             return MovimentosPossiveis()[posicao.Linha, posicao.Coluna];
         }
